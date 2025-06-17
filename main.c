d045428a0dff9f0428b1d95d17a34f28f9f0d7a8
Rui Fazendeiro <ruifazendeiro@outlook.pt>
2025-06-17 22:08:13 +0100

    Atualiza lógica principal em main.c

--- main.c
+++ main.c
@@ -5,9 +5,26 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 100
+#define MAX 100  // Máximo de mesas e clientes que esperamos ler
 
-Mesa mesas[MAX];
-Cliente clientes[MAX];
-int numMesas = 0 , numClientes = 0;
+Mesa mesas[MAX];         // Array para guardar mesas
+Cliente clientes[MAX];   // Array para guardar os grupos de clientes
 
+int nMesas = 0;          // Contador de mesas lidas do ficheiro
+int nClientes = 0;       // Contador de clientes lidos do ficheiro
+
+int main ()
+{
+
+    // Abre os ficheiros de mesas e clientes
+FILE *fMesas = fopen("mesas.csv", "r");
+FILE *fClientes = fopen("clientes.csv", "r");
+
+// Verificação de erro ao abrir os ficheiros
+if (!fMesas || !fClientes) 
+{
+    printf("Erro ao abrir ficheiros.\n");
+    return 1;  // Termina o programa com erro
+}
+
+}
